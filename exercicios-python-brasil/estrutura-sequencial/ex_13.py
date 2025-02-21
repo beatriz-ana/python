3c755a70c38dbd649a1da37e44575f03b6b6bef9
Ana Beatriz <anabeatrizbarbosasilva7@gmail.com>
2025-02-21 16:41:40 -0300

    Update ex_13.py

--- exercicios-python-brasil/estrutura-sequencial/ex_13.py
+++ exercicios-python-brasil/estrutura-sequencial/ex_13.py
@@ -2,14 +2,9 @@
 Para homens: (72.7*h) - 58
 Para mulheres: (62.1*h) - 44.7'''
 
-sexo = int(input('Escolha: 1- Sexo Masculino / 2- Sexo Feminino: ')) 
 altura = float(input('Informe sua altura:'))
 
-peso_ideal = (72.7*altura) - 58 if sexo == 1 else (62.1*altura) - 44.7
+peso_ideal_homem = (72.7*altura) - 58 
+peso_ideal_mulher = (62.1*altura) - 44.7
 
-if sexo == 1:
-    print('Seu peso ideal é: ', peso_ideal)
-elif sexo == 2:
-    print('Seu peso ideal é: ', peso_ideal)
-else:
-    print('Entrada inválida!')
+print(f'O peso ideal para essa altura é \nSexo Masculino:{peso_ideal_homem}\nSexo Feminino: {peso_ideal_mulher}')
