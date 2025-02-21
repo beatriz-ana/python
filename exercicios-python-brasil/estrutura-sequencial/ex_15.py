ab6adb75d50c58670c06e7c7cb8b8af29a417f5c
Ana Beatriz <anabeatrizbarbosasilva7@gmail.com>
2025-02-21 17:17:55 -0300

    Update ex_15.py

--- exercicios-python-brasil/estrutura-sequencial/ex_15.py
+++ exercicios-python-brasil/estrutura-sequencial/ex_15.py
@@ -8,4 +8,18 @@ calcule os descontos e o salário líquido, conforme a tabela abaixo:
 - IR (11%) : R$
 - INSS (8%) : R$
 - Sindicato ( 5%) : R$
-= Salário Liquido : R$'''
+= Salário Liquido : R$
+Obs.: Salário Bruto - Descontos = Salário Líquido.'''
+
+salario_por_hora = float(input('Informe quanto você ganha por hora: '))
+horas_trabalhadas = float(input('Quantas horas você trabalhou?'))
+
+salario_bruto = salario_por_hora * horas_trabalhadas
+
+ir = salario_bruto * 0.11
+inss = salario_bruto * 0.08
+sindicato = salario_bruto * 0.05
+
+salario_liquido = salario_bruto - ir - inss - sindicato
+
+print(f'\nSalario bruto: R${salario_bruto:.2f} \nIR(11%):R${ir:.2f} \nINSS(8%): R${inss:.2f} \nSindicato(5%): R${sindicato:.2f} \nSalario Líquido: R${salario_liquido:.2f}')
