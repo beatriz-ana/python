74e7a03e02b26090c80f661f1e775468cae5f4a7
Ana Beatriz <anabeatrizbarbosasilva7@gmail.com>
2025-02-21 17:00:01 -0300

    updates

--- exercicios-python-brasil/estrutura-sequencial/ex_12.py
+++ exercicios-python-brasil/estrutura-sequencial/ex_12.py
@@ -4,4 +4,4 @@ altura= float(input('Informe sua altura: '))
 
 peso_ideal = (72.7 * altura) - 58
 
-print('Seu peso ideal é : ', peso_ideal)
+print(f'Seu peso ideal é : {peso_ideal:.2f}')
--- exercicios-python-brasil/estrutura-sequencial/ex_13.py
+++ exercicios-python-brasil/estrutura-sequencial/ex_13.py
@@ -1,4 +1,4 @@
-'''Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas:
+'''13)Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas:
 Para homens: (72.7*h) - 58
 Para mulheres: (62.1*h) - 44.7'''
 
@@ -7,4 +7,4 @@ altura = float(input('Informe sua altura:'))
 peso_ideal_homem = (72.7*altura) - 58 
 peso_ideal_mulher = (62.1*altura) - 44.7
 
-print(f'O peso ideal para essa altura é \nSexo Masculino:{peso_ideal_homem}\nSexo Feminino: {peso_ideal_mulher}')
+print(f'O peso ideal para essa altura é \nSexo Masculino:{peso_ideal_homem:.2f}\nSexo Feminino: {peso_ideal_mulher:.2f}')
